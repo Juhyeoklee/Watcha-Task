65463a9bba2db59c2d7f4b8c3ef0b94e74d941e8
Juhyeoklee <gur6531@naver.com>
2021-02-05 01:05:02 +0900

    update: waterfall collectionview layout

---
+++ Watcha-Task/Watcha-Task/Source/Cells/GIFImageCVCell.swift
@@ -0,0 +1,39 @@
+//
+//  GIFImageCVCell.swift
+//  Watcha-Task
+//
+//  Created by 이주혁 on 2021/02/04.
+//
+
+import UIKit
+
+class GIFImageCVCell: UICollectionViewCell {
+    static let identifier = "GIFImageCVCell"
+    
+    var gifImageView: UIImageView = {
+        var imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleToFill
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initLayout()
+        print(#function)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func initLayout() {
+        self.addSubview(gifImageView)
+        NSLayoutConstraint.activate([
+            gifImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            gifImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            gifImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            gifImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+}
--- Watcha-Task/Watcha-Task/Source/VCs/SearchVC.swift
+++ Watcha-Task/Watcha-Task/Source/VCs/SearchVC.swift
@@ -12,7 +12,24 @@ class SearchVC: UIViewController {
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var searchButton: UIButton!
     
+    @IBOutlet weak var searchResultCollectionView: UICollectionView! {
+        didSet {
+            searchResultCollectionView.delegate = self
+            searchResultCollectionView.dataSource = self
+            searchResultCollectionView.register(GIFImageCVCell.self,
+                                                forCellWithReuseIdentifier: GIFImageCVCell.identifier)
+            if let layout = searchResultCollectionView.collectionViewLayout as? WaterFallCollectionViewLayout {
+                layout.delegate = self
+            }
+        }
+    }
     // MARK:- Member Variation
+    // CollectionView Space 
+    let lineSpacing: CGFloat = 5
+    let itemSpacing: CGFloat = 5
+    let horizontalSpacing: CGFloat = 5
+    let verticalSpacing: CGFloat = 10
+    var gifImageSizes: [CGFloat] = [100, 200, 50, 30, 90, 100, 100, 200, 300, 150, 200, 300, 100, 200, 300]
     
     
     // MARK:- LifeCycle Method
@@ -28,3 +45,50 @@ class SearchVC: UIViewController {
         searchTextField.makeRounded(cornerRadius: 20)
     }
 }
+
+// MARK:- UICollectionViewDataSource Extensions
+extension SearchVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GIFImageCVCell.identifier, for: indexPath) as? GIFImageCVCell else {
+            return UICollectionViewCell()
+        }
+//        print(indexPath)
+        cell.gifImageView.backgroundColor = .brown
+        return cell
+    }
+}
+
+// MARK:- UICollectionViewDelegate Extensions
+extension SearchVC: UICollectionViewDelegate {
+    
+}
+// MARK:- WaterFallCollectionViewLayoutDelegate Extensions
+extension SearchVC: WaterFallCollectionViewLayoutDelegate {
+    func collectionView(_ collectionView: UICollectionView, heightForImageAtIndexPath indexPath: IndexPath) -> CGFloat {
+        return gifImageSizes[indexPath.item]
+    }
+}
+
+//// MARK:- UICollectionViewDelegateFlowLayout Extensions
+//extension SearchVC: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        print(indexPath)
+//        return CGSize(width: (collectionView.frame.width - itemSpacing - 2 * horizontalSpacing )/2, height: arr[indexPath.item])
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+//        return itemSpacing
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return lineSpacing
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return UIEdgeInsets(top: verticalSpacing, left: horizontalSpacing, bottom: verticalSpacing, right: horizontalSpacing)
+//    }
+//}
